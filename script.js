071ec7d42904bf85a7f414ff6311a5f5653b3c50
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-06-01 23:44:07 +0200

    Add start game function

---
+++ script.js
@@ -0,0 +1,21 @@
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+const startGame = () => {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+  }
+  console.log(message)
+}
+
+
